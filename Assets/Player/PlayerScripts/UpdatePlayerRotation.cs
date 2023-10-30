6a905fa653349b1490ac903bb62cda2a31ffe9f0
Alex M <almaynard2404@gmail.com>
2023-10-30 17:20:52 -0500

    [ADD] & [MOD] Added Jump and refactoring to PlayerController
    Added code in the PlayerController for Jumping and refactored movement code for the player into multiple switch statements. Also added reduced movement while in the air for the player.

--- Assets/Player/PlayerScripts/UpdatePlayerRotation.cs
+++ Assets/Player/PlayerScripts/UpdatePlayerRotation.cs
@@ -5,10 +5,12 @@
     Last Modified Date: October 27, 2023
     Program Description: This script handles the camera movement of the player and rotation of the player 
     object as well (to make the camera and player X rotations the same).
-    Revision History:   
-        -> Transfered this code from CameraController
-        -> modified the code added to work in this script and removed the start
-        -> Added more comments
+    Revision History:  
+        -Ocotober 27, 2023:
+            -> Transfered this code from CameraController
+            -> modified the code added to work in this script and removed the start
+            -> Added more comments
+            -> Changed to FixedUpdate for more consistent Lerp to adjust player rotation.
  */
 
 using System.Collections;
@@ -25,7 +27,7 @@ public class UpdatePlayerRotation : MonoBehaviour
     public GameObject cam;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //New Quanterion that is updated to match the camera's Y and the players current other values as we do not want to update those
         Quaternion playerNewRotationX = new Quaternion(this.transform.rotation.x, cam.transform.rotation.y, this.transform.rotation.z, cam.transform.rotation.w);
