35a76fc0f390e1b7a8305a16e9828a1cc31f88ba
AudreyBernierLarose <abernier@my.centennialcollege.ca>
2023-11-11 09:07:29 -0500

    Initial implementation of the scoring system

---
+++ Assets/_Scripts/ScoreSystem/Target.cs
@@ -0,0 +1,42 @@
+/*
+ * 
+    Author's Name:          Audrey Bernier Larose
+    Last Modified By:       Audrey Bernier Larose
+    Last Date Modified:     November 11, 2023
+    Program Description:    Sets the type of target to the object.
+    Revision History:       November 11, 2023: Initial script and documentation.                            
+ */
+
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public TargetType type = TargetType.NONE;
+    public int multiplier = 1;
+
+    /// <summary>
+    /// Checks whether the other object is a bullet coming from the player.
+    /// If it is, it adjusts the player score, and enemy's health.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerBullet")) {
+            if(multiplier > 0)  ScoreManager.Add((int)type * multiplier);
+            else  ScoreManager.Add((int)type);
+
+            gameObject.transform.root.gameObject.GetComponent<EnemyController>().health -= (int)type;
+        }
+    }
+}
+
+/// <summary>
+/// Types of target possible.
+/// </summary>
+public enum TargetType { 
+    NONE = 0,
+    HEAD = 50,
+    BODY = 20,
+    ARM = 15,
+    LEG = 10
+}
