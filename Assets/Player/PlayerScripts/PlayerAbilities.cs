d0558b40c63286c2481be1f7713ff6b285fbf2dd
Alex M <almaynard2404@gmail.com>
2023-11-25 13:05:38 -0600

    [MOD] refactored the PLayerAbilities script
    Refactored OnPhase, OnTeleport and comments for them. Also added initial comments to the cooldown method.

--- Assets/Player/PlayerScripts/PlayerAbilities.cs
+++ Assets/Player/PlayerScripts/PlayerAbilities.cs
@@ -4,7 +4,7 @@
     Author's Name: Alexander  Maynard
     Creation Date: October 23, 2023
     Last Modified By: Alexander Maynard
-    Last Modified Date: November 24, 2023
+    Last Modified Date: November 25, 2023
     Program Description: This is script manages the PlayerAbilities using the StateMachine script implementation. 
     Furthermore, the states managed in this file are AbilitiesReady, Phase, Teleport and Cooldown.
     There are state checks, handlers for each state and respective states that are called depending on what conditions are true and what input is pressed.
@@ -43,12 +43,18 @@
     -November 24, 2023
         -> Uncommented code for teleport and finished it's implementation here
         ->Tied the teleport implementation to the UI (Ui images and text)
+    -November 25, 2023
+        -> Refactored OnPhase, OnTeleport and comments.
+        -> Also added initial comments to the cooldown method.
  */
 
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Playables;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using static UnityEditor.PlayerSettings;
@@ -180,8 +186,9 @@ public class PlayerAbilities : MonoBehaviour
         Phase();
 
         
-        //instantly calls the cooldown state as the cooldown state handles the ability ending.
-        playerAbilitesStateMachine.ChangeState(cooldown);
+        //if canPhase is true (phase is active) it calls the cooldown state as the cooldown state handles the ability ending.
+        if(canPhase == true)
+            playerAbilitesStateMachine.ChangeState(cooldown);
     }
 
     /// <summary>
@@ -220,8 +227,9 @@ public class PlayerAbilities : MonoBehaviour
         // Call teleport(happens once before the exit condition is called)
         Teleport();
 
-        //instantly calls the cooldown state as the cooldown state handles the ability ending.
-        playerAbilitesStateMachine.ChangeState(cooldown);
+        //if teleported is true (player teleported) it calls the cooldown state as the cooldown state handles the ability ending.
+        if (teleported == true)
+            playerAbilitesStateMachine.ChangeState(cooldown);
     }
 
     /// <summary>
@@ -262,6 +270,8 @@ public class PlayerAbilities : MonoBehaviour
 
 
     //timer to be used for both ability cooldowns and phase ending
+    //cooldown state handles the abilities ending and decommissioning the abitiy changes to their previous states 
+    //depending on the timer.
     private void Cooldown()
     {
         Debug.Log("Ability is cooling down");
