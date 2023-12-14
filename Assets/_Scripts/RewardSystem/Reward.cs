c7289ef1926c598e50574f31acb46baa31d42234
AudreyBernierLarose <abernier@my.centennialcollege.ca>
2023-12-13 19:44:45 -0500

    feat: added sfx on reward when player triggers it.

--- Assets/_Scripts/RewardSystem/Reward.cs
+++ Assets/_Scripts/RewardSystem/Reward.cs
@@ -2,12 +2,14 @@
  * 
     Author's Name:          Audrey Bernier Larose
     Last Modified By:       Audrey Bernier Larose
-    Last Date Modified:     November 25, 2023
+    Last Date Modified:     December 13, 2023
     Program Description:    Class used to describe a reward
     Revision History:       October 25, 2023: Initial script and documentation.                            
+                            December 13, 2023: Added sound when player enters in the reward trigger box.
  */
 
 using UnityEngine;
+using System.Collections;
 
 public class Reward : MonoBehaviour
 {
@@ -18,7 +20,7 @@ public class Reward : MonoBehaviour
     private RewardManager reward_manager;
     private ScoreManager score_manager;
     private HealthManager health_manager;
-    
+    private AudioSource audioSource;
 
     /// <summary>
     /// Start method triggered by unity. This method is used to initialize the managers.
@@ -28,6 +30,7 @@ public class Reward : MonoBehaviour
         reward_manager = GameObject.Find("RewardManager").GetComponent<RewardManager>();
         score_manager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
         health_manager = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     /// <summary>
@@ -37,26 +40,34 @@ public class Reward : MonoBehaviour
     /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player")) this.Action();  
+        if (other.gameObject.CompareTag("Player")) 
+            StartCoroutine(WaitForAudio());                               
+    }
+
+    IEnumerator WaitForAudio()
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(1.0f);
+        Action();
     }
 
     /// <summary>
     /// Method that handles the proper action based on the reward type.
     /// </summary>
-    private void Action() { 
+    private void Action() {
         switch (type)
         {
-            case RewardType.HEALTH:                
+            case RewardType.HEALTH:
                 reward_manager.GetComponent<RewardManager>().health_manager.Add_Health(worth);
                 Destroy(gameObject);
                 break;
 
-            case RewardType.BONUS:                
+            case RewardType.BONUS:
                 score_manager.Add(worth);
                 Destroy(gameObject);
                 break;
 
-            case RewardType.IMMUNE:
+            case RewardType.IMMUNE:                
                 Destroy(gameObject);
                 health_manager.Toggle_Is_Immune();
                 break;
